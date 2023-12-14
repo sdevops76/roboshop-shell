e3ebc8ef07237171e5c9184db6d9af94edb30de6
joindevopscloud <info@joindevops.com>
2023-12-14 08:38:45 +0530

    roboshop shell scripts

--- catalogue.sh
+++ catalogue.sh
@@ -42,11 +42,18 @@ dnf install nodejs -y  &>> $LOGFILE
 
 VALIDATE $? "Installing NodeJS:18"
 
-useradd roboshop
+id roboshop
+if [ $? -ne 0 ]
+then
+    useradd roboshop
+    VALIDATE $? "roboshop user creation"
+else
+    echo -e "roboshop user already exist $Y SKIPPING $N"
+fi
 
 VALIDATE $? "creating roboshop user"
 
-mkdir /app
+mkdir -p /app
 
 VALIDATE $? "creating app directory"
 
@@ -56,7 +63,7 @@ VALIDATE $? "Downloading catalogue application"
 
 cd /app 
 
-unzip /tmp/catalogue.zip  &>> $LOGFILE
+unzip -o /tmp/catalogue.zip  &>> $LOGFILE
 
 VALIDATE $? "unzipping catalogue"
 
