cdd9739eed1916a0a93c1f63bdcfa3e2b57be843
joindevopscloud <info@joindevops.com>
2023-12-15 07:47:38 +0530

    daws-76s

--- redis.sh
+++ redis.sh
@@ -43,7 +43,7 @@ dnf install redis -y
 
 VALIDATE $? "Installing Redis"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
 
 VALIDATE $? "allowing remote connections"
 
